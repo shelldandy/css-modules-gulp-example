711ea40508cf7ad1433c00514fe19ef23cf02c03
Miguel Palau <mpalauzarza@gmail.com>
2018-01-01 13:55:21 -0600

    eslint fix

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,5 +1,5 @@
-const gulp    = require('gulp')
-const config  = require('../config')
+const gulp = require('gulp')
+const config = require('../config')
 const when = require('gulp-if')
 const $ = require('gulp-load-plugins')()
 const production = config.production
@@ -19,28 +19,28 @@ gulp.task('main:styles', () =>
     .pipe(gulp.dest(config.directories.dist.styles))
 
     .pipe(when(production, $.rename({suffix: '.min'})))
-    .pipe(when(production, $.purifycss( config.purify, { info: true } )))
+    .pipe(when(production, $.purifycss(config.purify, { info: true })))
     .pipe(when(production, $.cssnano()))
     .pipe(when(production, gulp.dest(config.directories.dist.styles)))
 )
 
 gulp.task('vendor:styles', () =>
   gulp.src(config.project.cssVendorFile)
-  .pipe(when(!production, $.sourcemaps.init()))
-  .pipe($.sass({importer: moduleImporter()}))
-  .on('error', $.sass.logError)
-  .pipe($.autoprefixer({
-    browsers: config.browsers
-  }))
-  .pipe(when(production, $.groupCssMediaQueries()))
-  .pipe(when(production, $.csscomb()))
-  .pipe(when(!production, $.sourcemaps.write('./')))
-  .pipe(gulp.dest(config.directories.dist.styles))
+    .pipe(when(!production, $.sourcemaps.init()))
+    .pipe($.sass({importer: moduleImporter()}))
+    .on('error', $.sass.logError)
+    .pipe($.autoprefixer({
+      browsers: config.browsers
+    }))
+    .pipe(when(production, $.groupCssMediaQueries()))
+    .pipe(when(production, $.csscomb()))
+    .pipe(when(!production, $.sourcemaps.write('./')))
+    .pipe(gulp.dest(config.directories.dist.styles))
 
-  .pipe(when(production, $.rename({suffix: '.min'})))
-  .pipe(when(production, $.purifycss( config.purify, { info: true } )))
-  .pipe(when(production, $.cssnano()))
-  .pipe(when(production, gulp.dest(config.directories.dist.styles)))
+    .pipe(when(production, $.rename({suffix: '.min'})))
+    .pipe(when(production, $.purifycss(config.purify, { info: true })))
+    .pipe(when(production, $.cssnano()))
+    .pipe(when(production, gulp.dest(config.directories.dist.styles)))
 )
 
 gulp.task('styles', gulp.parallel('main:styles', 'vendor:styles'))
