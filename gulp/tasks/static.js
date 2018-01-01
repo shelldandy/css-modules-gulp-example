3177271f53d3b88b01ad74c32a6d2f2d9dfb9575
Miguel Palau <mpalauzarza@gmail.com>
2018-01-01 16:40:02 -0600

    clean modules

--- gulp/tasks/static.js
+++ gulp/tasks/static.js
@@ -3,11 +3,13 @@ const config = require('../config')
 const del = require('del')
 
 gulp.task('clean', () =>
-  del([config.directories.dist.base])
+  del([
+    config.directories.dist.base,
+    config.directories.src.cssModules
+  ])
 )
 
 gulp.task('images', () =>
   gulp.src(config.directories.src.images + '/**/*')
     .pipe(gulp.dest(config.directories.dist.images))
 )
-
