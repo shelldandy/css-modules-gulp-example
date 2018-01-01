b1f2d463ed52135c8a71fe18e8cf3eab8a9496b2
Miguel Palau <mpalauzarza@gmail.com>
2018-01-01 16:20:24 -0600

    html modules

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -4,6 +4,10 @@ const $ = require('gulp-load-plugins')()
 const production = config.production
 const fs = require('fs')
 
+const POSTHTML_PLUGINS = [
+  require('posthtml-css-modules')(`${config.directories.src.cssModules}/`)
+]
+
 gulp.task('markup', () =>
   gulp.src(config.directories.src.markup + '/*.pug')
     .pipe($.pug({
@@ -13,6 +17,7 @@ gulp.task('markup', () =>
         production
       }
     })).on('error', config.onError)
+    .pipe($.posthtml(POSTHTML_PLUGINS))
     .pipe($.htmlPrettify())
     .pipe(gulp.dest(config.directories.dist.markup))
 )
