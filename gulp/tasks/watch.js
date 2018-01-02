131dd2816cbe0e7b8c20f8a5bdd50b2a248c9d8c
Miguel Palau <mpalauzarza@gmail.com>
2018-01-01 19:11:26 -0600

    split watch

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync')
+const config = require('../config')
+
+const reload = done => {
+  browserSync.reload()
+  done()
+}
+
+gulp.task('watch', done => {
+  // static files
+  gulp.watch(config.directories.src.markup + '/**/*.pug', gulp.series('markup', reload))
+  gulp.watch(config.directories.src.icons + '/**/*.svg', gulp.series('markup', reload))
+  gulp.watch(config.directories.src.images + '/**/*', gulp.series('images', reload))
+  // scripts
+  gulp.watch(config.directories.src.scripts + '/**/*.js', gulp.series('scripts', reload))
+  // Fonts
+  gulp.watch(config.project.fontFiles, gulp.series('fonts', reload))
+  // styles
+  gulp.watch(config.directories.src.styles + '/**/*.scss', gulp.series('styles', reload))
+  done()
+})
