50578552cb89c195383329f6d0bb8fabe38bf465
Miguel Palau <mpalauzarza@gmail.com>
2018-01-06 21:51:24 -0600

    compress main gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const config = require('./gulp/config')
 const requireDir = require('require-dir')
 const gulp = require('gulp')
 
-// Add all the tasks and files, boom!
 requireDir('./gulp/tasks', {recurse: true})
 
 let tasks = [
@@ -23,7 +22,6 @@ if (config.production) {
 }
 
 gulp.task('build', gulp.series(...tasks))
-
 gulp.task('serve', gulp.parallel('browser-sync', 'watch'))
 gulp.task('release', gulp.series('build', 'zip'))
 gulp.task('default', gulp.series('build', 'serve'))
