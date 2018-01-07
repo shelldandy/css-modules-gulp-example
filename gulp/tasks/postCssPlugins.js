88fa9efbda1eac312cc55e044d2a496b03b1a020
Miguel Palau <mpalauzarza@gmail.com>
2018-01-07 00:38:00 -0600

    to be continued...

--- gulp/tasks/postCssPlugins.js
+++ gulp/tasks/postCssPlugins.js
@@ -1,23 +1,28 @@
 const config = require('../config')
 
+let json = {}
+
 const plugins = [
   require('autoprefixer')({ browsers: config.browsers }),
   require('postcss-modules')({
     generateScopedName: config.production ? '[hash:base64:5]' : '[name]__[local]___[hash:base64:5]',
     getJSON: function (cssPath, json) {
-      const fs = require('fs-path')
       const path = require('path')
+      // An array of directories
       const exploded = cssPath.split(path.sep)
+      // but we only need starting from the main styles directory
       const mainIndex = exploded.indexOf('main')
-      const rest = exploded.slice(mainIndex + 1)
-      rest.pop()
+      let dirs = exploded.slice(mainIndex + 1)
+      // remove the css filename with extension
+      dirs.pop()
+      // add again but without extension
       const cssName = path.basename(cssPath, '.css')
-      rest.push(cssName)
-      const finalString = rest.join('')
-      const jsonFileName = path.resolve(`./${config.directories.src.cssModules}/${finalString}.json`)
-      fs.writeFileSync(jsonFileName, JSON.stringify(json))
+      dirs.push(cssName)
+      // TODO: Foreach item in the directories
+      // create a single object recursively into json
     }
   })
 ]
 
 module.exports = plugins
+exports.checkJson = () => json
